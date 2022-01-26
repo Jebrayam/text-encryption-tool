f1e636adf586432e45355dd02f2b8ce62a9f0f2f
Brayam Andres Saavedra Arce <brayamsaavedra@gmail.com>
2022-01-26 14:57:20 -0500

    Personalización de la pagina web

--- js/changeMode.js
+++ js/changeMode.js
@@ -1,23 +1,4 @@
-var encryptField = document.querySelector("#encrypt");
-var decryptField = document.querySelector("#decrypt");
-
-encryptField.addEventListener("click", function(){
-    encryptField.style.backgroundColor =  'rgb(89, 255, 47)';
-    encryptField.style.color = "black";
-    decryptField.style.backgroundColor =  'rgba(0, 0, 0, 0)';
-    decryptField.style.color = 'rgb(89, 255, 47)';
-
-});
-
-decryptField.addEventListener("click", function(){
-    decryptField.style.backgroundColor =  'rgb(89, 255, 47)';
-    decryptField.style.color = "black";
-    encryptField.style.backgroundColor =  'rgba(0, 0, 0, 0)';
-    encryptField.style.color = 'rgb(89, 255, 47)';
- 
-});
-
-encryptField.style.backgroundColor =  'rgb(89, 255, 47)';
-    encryptField.style.color = "black";
-    decryptField.style.backgroundColor =  'rgba(0, 0, 0, 0)';
-    decryptField.style.color = 'rgb(89, 255, 47)';
+function auto_height(elem) {  /* javascript */
+    elem.style.height = "1px";
+    elem.style.height = (elem.scrollHeight)+"px";
+}
